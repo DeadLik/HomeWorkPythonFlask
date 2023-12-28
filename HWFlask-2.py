a0414b7ba76da71cf20d88332884bf6af98f7c61
Игорь <mc_grove@bk.ru>
2023-12-28 15:27:09 +0300

    HW-2 finish

--- HWFlask-2.py
+++ HWFlask-2.py
@@ -1,37 +1,30 @@
-from flask import Flask, render_template, request, redirect, make_response
+from flask import Flask, render_template, request, redirect, make_response, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', "POST"])
 def index():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        resp = make_response(redirect(url_for("hello")))
+        resp.set_cookie("name", name)
+        resp.set_cookie("email", email)
+        return resp
     return render_template("index.html")
 
 
-@app.route("/login", methods=["POST"])
-def login():
-    name = request.form.get("name")
-    email = request.form.get("email")
-
-    resp = make_response(redirect("/hello"))
-    resp.set_cookie("name", name)
-    resp.set_cookie("email", email)
-    return resp
-
-
-@app.route("/hello")
+@app.route("/hello", methods=['GET', "POST"])
 def hello():
+    if request.method == 'POST':
+        response = make_response(redirect(url_for("index")))
+        response.delete_cookie("name")
+        response.delete_cookie("email")
+        return response
     name = request.cookies.get("name")
     email = request.cookies.get("email")
     return render_template("hello.html", name=name, email=email)
-
-
-@app.route("/logout")
-def logout():
-    response = make_response(redirect("/"))
-    response.delete_cookie("name")
-    response.delete_cookie("email")
-    return response
 
 
 if __name__ == "__main__":
